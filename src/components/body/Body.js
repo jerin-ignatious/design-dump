7b2dd579c5e8c6655ec0d0d6fd64824ba4847ba3
jerin-ignatious <jerinignatious1998@gmail.com>
2021-10-10 17:47:16 +0530

    GRFT-104 | Modal and slider added
    The landing page now has an image slider at the top and the form has been moved inside a modal

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactModal from 'react-modal';
 import './body.css'
+import Slider from './slider/Slider'
 import UploadForm from './uploadForm/UploadForm';
 
 export default function Body() {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
     return (
         <div className='body'>
-            <UploadForm/>
+            <Slider/>
+            <button 
+                className = 'modal-button'
+                onClick={() => setModalIsOpen(true)}
+            >
+                Upload Design
+            </button>
+            <ReactModal 
+                isOpen={modalIsOpen}
+                onRequestClose={() => setModalIsOpen(false)}
+            >
+                <div className='modal'>
+                    <div>
+                        <UploadForm/>
+                    </div>
+                    <div>
+                        <button className = 'close-button'
+                            onClick={() => setModalIsOpen(false)}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </ReactModal>
         </div>
     )
 }
---
+++ src/data/sliderImages.js
@@ -0,0 +1,22 @@
+export const imagesData = [
+    {
+        id: 1,
+        img:'https://i.guim.co.uk/img/media/8c7f4fe66d305fb86fc3246dd47a9c06d216f7ec/0_139_1268_761/master/1268.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=fe94855e2d15136665afd1f77177b125',
+    },
+    {
+        id: 2,
+        img:'https://i1.sndcdn.com/artworks-000176169955-m83wru-t500x500.jpg',
+    },
+    {
+        id: 3,
+        img:'https://www.prospectmagazine.co.uk/content/uploads/2016/11/Pepedankmeems420blazeitohbabyatripledankaestheticafkeepit100.jpg',
+    },
+    {
+        id: 4,
+        img:'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+    },
+    {
+        id: 5,
+        img:'https://i.guim.co.uk/img/media/327e46c3ab049358fad80575146be9e0e65686e7/0_0_1023_742/master/1023.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=331a1bc4f5d7a96a6131f4f142065662',
+    },
+];
